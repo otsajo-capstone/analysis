6c795c2c19097e823ee0e9e86e34dd35708509e5
Chaehwan Lim <chlim428@gmail.com>
2020-10-19 11:50:29 +0900

    Transplant cloth recognizer from cloth image

---
+++ README.md
@@ -0,0 +1,8 @@
+pip install
+- flask
+- requests
+- bs4
+- selenium
+- image
+- opencv-python
+- tensorflow
--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from scrap import scrap_by_bs, scrap_by_selenium
 import requests
 import json
+from cloth_recognizer import cloth_recognizer
 
 app = Flask(__name__)
 
@@ -22,14 +23,35 @@ def scraper():
             if len(result) == 0:
                 return {'status': 'scrap failed'}
 
+        cloth_recognizer()
+
         return {'status': 'success', 'image_urls': list(result)}
 
     except Exception as e:
         return {'error': str(e)}
 
-@app.route('/url/select', methods=['post'])
+@app.route('/url/select', methods=['POST'])
 def selecter():
-    print('')
+    try:
+        url = request.form['url']
+        min_width = int(request.form['width'])
+        min_height = int(request.form['height'])
+        selected_index = request.form['selected']
+        selected_index = list(map(int, selected_index))
+
+        result = scrap_by_bs(url, min_width, min_height)
+        if len(result) == 0:
+            result = scrap_by_selenium(url, min_width, min_height)
+
+            if len(result) == 0:
+                return {'status': 'scrap failed'}
+
+        result = list(result)
+        result = [result[i] for i in selected_index]
+        return {'status': 'success', 'image_urls': result}
+
+    except Exception as e:
+        return {'error': str(e)}
 
 if __name__ == '__main__':
     app.run(debug=True)
