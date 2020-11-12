12807da571004bbe5f3b388c90204a4fde60cf94
Chaehwan Lim <chlim428@gmail.com>
2020-11-12 23:42:15 +0900

    Redesign KMeans Algorithm to ignore transparent pixels

--- color_analyzer.py
+++ color_analyzer.py
@@ -1,49 +1,121 @@
 import numpy as np
-from sklearn.cluster import KMeans
 import cv2
-import matplotlib.pyplot as plt
+from copy import deepcopy
 
-def centroid_histogram(clt):
-    # grab the number of different clusters and create a histogram
-    # based on the number of pixels assigned to each cluster
-    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+'''
+image: one-dimensional array which consists of 3-channel(RGB) element
+cluster: the number of group to classify
 
-    # normalize the histogram, such that it sums to one
-    hist = hist.astype("float")
-    hist /= hist.sum()
+this custom_KMeans clusters color code ignoring true black code(0, 0, 0) 
+'''
+def custom_KMeans(image_pixels, cluster):
+    # Min and Max for RGB
+    min_c = 0
+    max_c = 255
 
-    # return the histogram
-    return hist
+    # Generate random color points in RGB
+    centroids_r = [100, 150, 200]
+    centroids_g = [100, 150, 200]
+    centroids_b = [100, 150, 200]
+    centroids = np.array(list(zip(centroids_r, centroids_g, centroids_b)))
 
-def plot_colors(hist, centroids):
-    # initialize the bar chart representing the relative frequency
-    # of each of the colors
-    bar = np.zeros((50, 300, 3), dtype="uint8")
-    startX = 0
+    def euclidean_distance(center, target):
+        return (center[0] - target[0]) ** 2 + \
+               (center[1] - target[1]) ** 2 + \
+               (center[2] - target[2]) ** 2
 
-    # loop over the percentage of each cluster and the color of
-    # each cluster
-    for (percent, color) in zip(hist, centroids):
-        # plot the relative percentage of each cluster
-        endX = startX + (percent * 300)
-        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-                      color.astype("uint8").tolist(), -1)
-        startX = endX
+    centroids_old = np.zeros(centroids.shape)
+    labels_for_pixels = np.full(len(image_pixels), cluster)
+    labels_counter = np.zeros(cluster)
+    error = np.zeros(cluster)
 
-    # return the bar chart
-    return bar
+    # Initialize error
+    for i in range(cluster):
+        error[i] = euclidean_distance(centroids_old[i], centroids[i])
+
+    # 4. Iterate while error converges to 0
+    while error.all() != 0:
+        num_nonzero_pixels = deepcopy(len(image_pixels))
+        # 2. Classify pixels into group
+        for i in range(len(image_pixels)):
+            distances = np.zeros(cluster)
+
+            # Ignore zero pixels
+            if sum(image_pixels[i]) == 0:
+                continue
+
+            # calculate euclidean distance to find out appropriate color group
+            for j in range(cluster):
+                distances[j] = euclidean_distance(centroids[j], image_pixels[i])
+
+            cluster_idx = np.argmin(distances)
+            labels_for_pixels[i] = cluster_idx
+            labels_counter[cluster_idx] += 1
+
+        # 3. Update centroids for better clustering
+        centroids_old = deepcopy(centroids)
+        for i in range(cluster):
+            # Collecting ith label members
+            members = [image_pixels[j] for j in range(len(image_pixels))
+                       if labels_for_pixels[j] == i and sum(image_pixels[j]) != 0]
+            # Select new centroids
+            centroids[i] = np.mean(members, axis=0)
+
+        # Recalculate error
+        for i in range(cluster):
+            error[i] = euclidean_distance(centroids_old[i], centroids[i])
+
+    labels_counter_all = sum(labels_counter)
+    labels_ratio = [labels_counter[i]/labels_counter_all for i in range(len(labels_counter))]
+    centroids_hex = []
+    for center in centroids:
+        center_hex = []
+        for code in center:
+            if int(code) < 16:
+                center_hex.append("0" + hex(int(code)))
+            else:
+                center_hex.append(hex(int(code)))
+        center_hex = ''.join(center_hex)
+        center_hex = '#' + center_hex.replace("0x", "")
+        centroids_hex.append(center_hex)
+
+    centroids_hex = list(zip(centroids_hex, labels_ratio))
+    centroids_hex = sorted(centroids_hex, key=lambda x: x[1], reverse=True)
+
+    return centroids_hex
 
 def color_analyzer(original):
-    image = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(original, dsize=(0, 0), fx=0.3, fy=0.3, interpolation=cv2.INTER_AREA)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = image.reshape((image.shape[0] * image.shape[1], 3))
+    result = custom_KMeans(image, 3)
 
+    return result
+
+    '''
     k = 3
     clt = KMeans(n_clusters=k)
     clt.fit(image)
+    
+    rep_colors = []
+
+    i = 0
+    for center in clt.cluster_centers_:
+        center_ = center.tolist()
+        center_hex = []
+        for c in center_:
+            if int(c) < 16:
+                center_hex.append("0" + hex(int(c)))
+            else:
+                center_hex.append(hex(int(c)))
+        center_hex = ''.join(center_hex)
+        center_hex = '#' + center_hex.replace("0x", "")
+        rep_colors.append({'hex': center_hex, 'ratio': hist[i]})
+        i += 1
 
     hist = centroid_histogram(clt)
     hist = hist.tolist()
+    '''
 
     '''
     bar = plot_colors(hist, clt.cluster_centers_)
@@ -53,16 +125,3 @@ def color_analyzer(original):
     plt.imshow(bar)
     plt.show()
     '''
-
-    rep_colors = []
-
-    i = 0
-    for center in clt.cluster_centers_:
-        center_ = center.tolist()
-        center_hex = [hex(int(c)) for c in center_]
-        center_hex = ''.join(center_hex)
-        center_hex = '#' + center_hex.replace("0x", "")
-        rep_colors.append({'hex': center_hex, 'ratio': hist[i]})
-        i += 1
-
-    return rep_colors
